23bfc8eab1caaf768b2e70bd5292a39f40396608
beingsohailk <suhailkhann112@gmail.com>
2023-11-30 20:18:04 +0530

    Initial commit

---
+++ src/main/java/com/example/demofz/DemofzApplication.java
@@ -0,0 +1,13 @@
+package com.example.demofz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemofzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemofzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demofz/DemofzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demofz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemofzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
